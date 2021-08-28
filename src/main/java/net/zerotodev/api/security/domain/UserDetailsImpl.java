0de0cabf8280cff0cd70bdc35ae68649db199fd9
rlatmd0829 <rlatmd0829@naver.com>
2021-08-28 15:28:59 +0900

    21.08.28 스프링 시큐리티 provider 설정

--- src/main/java/net/zerotodev/api/security/domain/UserDetailsImpl.java
+++ src/main/java/net/zerotodev/api/security/domain/UserDetailsImpl.java
@@ -1,6 +1,7 @@
 package net.zerotodev.api.security.domain;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import net.zerotodev.api.user.domain.User;
 import org.springframework.security.core.GrantedAuthority;
@@ -10,7 +11,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import java.util.Collection;
 import java.util.List;
 import java.util.stream.Collectors;
-
+@Getter
 @RequiredArgsConstructor
 public class UserDetailsImpl implements UserDetails { // 도메인 구현체
 
@@ -22,45 +23,36 @@ public class UserDetailsImpl implements UserDetails { // 도메인 구현체
     private final String password; // json값은 무시 => 토큰값으로 판단한다는뜻
     private final Collection<? extends GrantedAuthority> authorities; // 권한은 여러개를 중복으로 줘서 collection으로 생성
 
-    public static UserDetailsServiceImpl build(User user){
+
+    public static UserDetailsImpl build(User user){
         List<GrantedAuthority> authorities = user.getRoles().stream() // role은 여러개라 stream 사용
                 .map(role -> new SimpleGrantedAuthority(role.getAuthority()))
                 .collect(Collectors.toList());
-        return null;
-    }
-
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        return null;
-    }
-
-    @Override
-    public String getPassword() {
-        return null;
-    }
-
-    @Override
-    public String getUsername() {
-        return null;
+        return new UserDetailsImpl(user.getUserId(),
+                user.getUsername(),
+                user.getName(),
+                user.getEmail(),
+                user.getPassword(),
+                authorities);
     }
 
     @Override
     public boolean isAccountNonExpired() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isAccountNonLocked() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isCredentialsNonExpired() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isEnabled() {
-        return false;
+        return true;
     }
 }
